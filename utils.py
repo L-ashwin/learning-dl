4a0e3c4d855dbda62d43ca587604d43cd46f83ac
L-ashwin <asblahorkar@gmail.com>
2020-12-02 21:07:27 +0530

    update docstring for load_cifar function

--- utils.py
+++ utils.py
@@ -1,9 +1,7 @@
 def load_cifar10(DIR):
     """
-    Each batch has array with 10000 rows, each row of the array stores a 32x32 colour image.
-    The first 1024 entries contain the red channel values, the next 1024 the green, and the final 1024 the blue.
-    The image is stored in row-major order,
-    so that the first 32 entries of the array are the red channel values of the first row of the image.
+    Input  - path to cifar10 directory
+    Output - X_train, y_train, X_test, y_test
     """
     import os
     import pickle
@@ -14,6 +12,10 @@ def load_cifar10(DIR):
         with open(os.path.join(DIR, f'data_batch_{batch}'), mode='rb') as file:
             data = pickle.load(file, encoding='latin1')
 
+            #Each batch has array with 10000 rows, each row of the array stores a 32x32 colour image.
+            #The first 1024 entries contain the red channel values, the next 1024 the green, and the final 1024 the blue.
+            #The image is stored in row-major order,
+            #so that the first 32 entries of the array are the red channel values of the first row of the image.
             imgs = data['data']
             imgs = np.reshape(a=imgs, newshape=(10000, 3, 32, 32), order='C').transpose(0, 2, 3, 1)
 
