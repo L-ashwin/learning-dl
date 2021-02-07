fbb3a4df1bbfbbc8285f77ef3e5d8d467027b1a8
l-ashwin <asblahorkar@gmail.com>
2021-02-07 20:39:21 +0530

    add plot settings

---
+++ Deep-Learning-NYU/res/plot.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+def set_default(figsize=(10, 10), dpi=100):
+    plt.style.use(['dark_background', 'bmh'])
+    plt.rc('axes', facecolor='k')
+    plt.rc('figure', facecolor='k')
+    plt.rc('figure', figsize=figsize, dpi=dpi)
